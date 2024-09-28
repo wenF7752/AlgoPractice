cb29228dd8d02d1fe7fe6031103c1cc81352177f
wenF7752 <113209938+wenF7752@users.noreply.github.com>
2024-09-27 22:12:17 -0400

    max char in string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+const estring = "abcccccccddaDDDDDDDDDD";
+
+function maxChar(str) {
+  let tempLowerCase = str.toLowerCase();
+  let charMap = {};
+  for (let char of tempLowerCase) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (let char in charMap) {
+    if (charMap[char] === Math.max(...Object.values(charMap))) {
+      return char;
+    }
+  }
+}
 
 module.exports = maxChar;
